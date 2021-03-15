218a7666a7ac601b3accf76facf0fda6fc2f07df
Silkypaladin <bednarski.szymon99@gmail.com>
2021-03-15 16:09:27 +0100

    Add population generating

--- genetic/population.py
+++ genetic/population.py
@@ -60,6 +60,13 @@ class Point:
             next_point.x += step
         return next_point
 
+    @staticmethod
+    def is_point_inside_coordinates(x, y, plate_x, plate_y):
+        if 0 < x < plate_x:
+            if 0 < y < plate_y:
+                return True
+        return False
+
     def __str__(self):
         return f"Point ({self.x}, {self.y})"
 
@@ -112,15 +119,15 @@ class Gene:
     def __init__(self):
         self.segments = list()
 
-    def generate_gene(self, start_point, end_point):
+    def generate_gene(self, start_point, end_point, plate_x, plate_y):
         current_p = Point(start_point.x, start_point.y)
         last_direction = -1
         while not current_p.__eq__(end_point):
-            new_data = self.generate_next_segment(current_p, end_point, last_direction)
+            new_data = self.generate_next_segment(current_p, end_point, last_direction, plate_x, plate_y)
             last_direction = new_data[1]
             current_p.update_point_coordinates(new_data[0])
 
-    def generate_next_segment(self, current_point, end_point, last_dir):
+    def generate_next_segment(self, current_point, end_point, last_dir, plate_x, plate_y):
         next_segment = Segment()
         current_dir = Direction.HORIZONTAL
         if current_point.x == end_point.x:
@@ -133,9 +140,8 @@ class Gene:
                 else:
                     next_segment.direction = Direction.UP
             else:
-                # TODO zastanowić sie nad dlugoscia tego odcinka (jak ja dobrac)
                 current_dir = Direction.HORIZONTAL
-                segment_len = random.randint(1, 3)
+                segment_len = random.randint(1, abs(plate_x - current_point.x))
                 random_dir = Direction.get_random_direction(Direction.LEFT, Direction.RIGHT)
                 next_segment.path_length = segment_len
                 next_segment.direction = random_dir
@@ -151,7 +157,7 @@ class Gene:
                     next_segment.direction = Direction.RIGHT
             else:
                 current_dir = Direction.VERTICAL
-                segment_len = random.randint(1, 3)
+                segment_len = random.randint(1, abs(plate_y - current_point.y))
                 random_dir = Direction.get_random_direction(Direction.UP, Direction.DOWN)
                 next_segment.path_length = segment_len
                 next_segment.direction = random_dir
@@ -233,16 +239,20 @@ class Gene:
     def calculate_outside_length(point_x, point_y, step, direction, plate_x, plate_y):
         length = 0
         for i in range(step):
+            next_x = point_x
+            next_y = point_y
             if direction == Direction.UP:
-                point_y += 1
+                next_y += 1
             if direction == Direction.DOWN:
-                point_y -= 1
+                next_y -= 1
             if direction == Direction.LEFT:
-                point_x -= 1
+                next_x -= 1
             if direction == Direction.RIGHT:
-                point_x += 1
-            if point_x > plate_x or point_y > plate_y or point_y <= 0 or point_x <= 0:
+                next_x += 1
+            if not (Point.is_point_inside_coordinates(point_x, point_y, plate_x, plate_y) and Point.is_point_inside_coordinates(next_x, next_y, plate_x, plate_y)):
                 length += 1
+            point_x = next_x
+            point_y = next_y
         return length
 
     def __str__(self):
@@ -257,54 +267,56 @@ class Individual:
     def __init__(self):
         self.genotype = list()
 
-    def generate_genotype(self, connections):
+    def generate_genotype(self, connections, plate_x, plate_y):
         for connection in connections:
             gene = Gene()
-            gene.generate_gene(connection.start_point, connection.end_point)
+            gene.generate_gene(connection.start_point, connection.end_point, plate_x, plate_y)
             self.genotype.append(gene)
 
     def get_intersection_points(self, connections):
         crossing_connections_by_point = dict()
-        crossing_connections = list()
+        intersection_points = list()
+        crossing_connections = set()
         for i in range(len(self.genotype)):
             visited_points = self.genotype[i].get_all_visited_points(connections[i].start_point)
             for point in visited_points:
                 if point in crossing_connections_by_point.keys():
                     crossing_connections_by_point[point].append(connections[i])
+                    crossing_connections.add(connections[i])
                 else:
                     crossing_connections_by_point[point] = list()
                     crossing_connections_by_point[point].append(connections[i])
         for cross in crossing_connections_by_point.values():
             if len(cross) > 1:
-                crossing_connections.append(cross)
-        return crossing_connections
+                intersection_points.append(cross)
+        return intersection_points, crossing_connections
 
     def calculate_total_path_length(self):
         total = 0
-        for gene in self.genotype():
+        for gene in self.genotype:
             total += gene.calculate_total_path_length()
+        return total
 
-    def get_total_outside_paths_number(self, start_point, plate_x, plate_y):
+    def get_total_outside_paths_number(self, connections, plate_x, plate_y):
         total = 0
-        for gene in self.genotype():
-            if gene.is_path_outside(start_point, plate_x, plate_y):
+        for i in range(len(self.genotype)):
+            if self.genotype[i].is_path_outside(connections[i].start_point, plate_x, plate_y):
                 total += 1
         return total
 
     def get_total_segments_amount(self):
         total = 0
-        for gene in self.genotype():
+        for gene in self.genotype:
             total += gene.get_segments_amount()
         return total
 
-    def get_total_outside_path_length(self, start_point, plate_x, plate_y):
+    def get_total_outside_path_length(self, connections, plate_x, plate_y):
         total = 0
-        for gene in self.genotype():
-            total += gene.get_outside_path_length(start_point, plate_x, plate_y)
+        for i in range(len(self.genotype)):
+            total += self.genotype[i].get_outside_path_length(connections[i].start_point, plate_x, plate_y)
         return total
 
-    def get_intersections_penalty(self, connections):
-        intersection_points = self.get_intersection_points(connections)
+    def get_intersections_penalty(self, intersection_points):
         total = 0
         point_penalty = 1.0
         for intersection in intersection_points:
@@ -314,46 +326,77 @@ class Individual:
             point_penalty = 1.0
         return total
 
+    def calculate_total_individual_penalty(self, connections, weights, intersection_points, plate_x, plate_y):
+        total = self.calculate_total_path_length() * weights['path_length']
+        total += self.get_total_segments_amount() * weights['segments_am']
+        total += self.get_total_outside_paths_number(connections, plate_x, plate_y) * weights['outside_number']
+        total += self.get_total_outside_path_length(connections, plate_x, plate_y) * weights['outside_length']
+        total += self.get_intersections_penalty(intersection_points) * weights ['intersections']
+
+        return total
+
     def print_genes(self):
         for gene in self.genotype:
             print(gene, "\n")
 
 
-class Solution:
+class Population:
     def __init__(self, plate_x=0, plate_y=0):
         self.plate_x = plate_x
         self.plate_y = plate_y
         self.connections = list()
         self.population = list()
+        self.weights = dict()
 
-    def get_connections(self):
-        return self.connections
+    def set_weights(self, weights):
+        self.weights = weights
+
+    def generate_population(self, individuals_num):
+        for i in range(individuals_num):
+            next_ind = Individual()
+            next_ind.generate_genotype(self.connections, self.plate_x, self.plate_y)
+            self.population.append(next_ind)
+
+    def find_best_individual(self):
+        for i in range(len(self.population)):
+            intersection_points, crossing_connections = self.population[i].get_intersection_points(self.connections)
+            print("Penalty for individual: ", i, ":", self.population[i].calculate_total_individual_penalty(self.connections, self.weights, intersection_points, self.plate_x, self.plate_y))
+            self.population[i].print_genes()
+
+
+class Solution:
+    def __init__(self):
+        self.population = Population()
 
     def read_solution_from_file(self, filename):
         with open(filename) as fp:
             for count, line in enumerate(fp):
                 data = line.rstrip('\n').split(';')
                 if count == 0:
-                    self.plate_x = int(data[0])
-                    self.plate_y = int(data[1])
+                    self.population.plate_x = int(data[0])
+                    self.population.plate_y = int(data[1])
                 else:
-                    self.connections.append(Connection(Point(int(data[0]), int(data[1])),
+                    self.population.connections.append(Connection(Point(int(data[0]), int(data[1])),
                                                        Point(int(data[2]), int(data[3]))))
 
     def __str__(self):
         conns = ""
-        for c in self.connections:
+        for c in self.population.connections:
             conns += c.__str__() + "\n"
 
-        return f"Plate: {self.plate_x}x{self.plate_y}, Connections:\n{conns}"
+        return f"Plate: {self.population.plate_x}x{self.population.plate_y}, Connections:\n{conns}"
 
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.read_solution_from_file('zad0.txt')
-    # print(solution)
+    solution.read_solution_from_file('./testdata/zad0.txt')
+    print(solution)
     ind = Individual()
-    ind.generate_genotype(solution.get_connections())
-    print(ind.get_intersections_penalty(solution.get_connections()))
-    # ind.print_genes()
+    ind.generate_genotype(solution.population.connections, solution.population.plate_x, solution.population.plate_y)
+    ind.print_genes()
+    solution.population.generate_population(10)
+    solution.population.set_weights({'path_length': 0.08, 'segments_am': 0.08, 'outside_number': 0.2, 'outside_length': 0.2, 'intersections': 0.3})
+    solution.population.find_best_individual()
+
+
 
