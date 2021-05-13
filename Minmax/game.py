eee46d5e55e388b0a0d1e752501dedf5d608cc78
Silkypaladin <bednarski.szymon99@gmail.com>
2021-05-13 12:59:49 +0200

    Implement 2 additional heuristics

--- Minmax/game.py
+++ Minmax/game.py
@@ -1,12 +1,13 @@
 import random
 import copy
 import math
-
+from timeit import default_timer as timer
 
 HOLES_AMOUNT = 14
 PLAYER_ONE = -1
 PLAYER_TWO = 0
 COUNTER_FIRST, COUNTER_SECOND = 0, 0
+MOVES_ONE, MOVES_TWO = 0, 0
 
 
 class Hole:
@@ -96,6 +97,22 @@ class Board:
             return True
         return False
 
+    @staticmethod
+    def h1_heuristic(board, starting_player):
+        if starting_player == PLAYER_ONE:
+            return board.holes[6].stones
+        else:
+            return board.holes[13].stones
+
+    @staticmethod
+    def heuristic_evaluate(board, number_of_moves, starting_player):
+        sign = 1
+        if starting_player == PLAYER_TWO:
+            sign = -1
+        right_hole = Board.h1_heuristic(board, starting_player)
+        # right_hole = 0
+        return 3 * (board.holes[7].stones - board.holes[0].stones) + (0.161 * right_hole * sign) + (0.29 * number_of_moves * sign)
+
     def add_remaining_stones_to_winners_well(self):
         start_hole = 1
         end_hole = 7
@@ -184,6 +201,7 @@ class Board:
         return self.holes[r].move_stones(self.player_turn)
 
     def ai_minimax_move(self, depth):
+        global MOVES_ONE, MOVES_TWO
         maximizing = True
         turn = 'Player\'s one choice: '
         if self.player_turn == PLAYER_TWO:
@@ -193,11 +211,14 @@ class Board:
         self.make_moves_from_list(chosen_moves)
         print(turn, best_val, chosen_moves)
         if self.player_turn == PLAYER_ONE:
+            MOVES_ONE += len(chosen_moves)
             self.player_turn = PLAYER_TWO
         else:
+            MOVES_TWO += len(chosen_moves)
             self.player_turn = PLAYER_ONE
 
     def ai_alpha_beta_move(self, depth):
+        global MOVES_ONE, MOVES_TWO
         maximizing = True
         turn = 'Player\'s one choice: '
         if self.player_turn == PLAYER_TWO:
@@ -207,8 +228,27 @@ class Board:
         self.make_moves_from_list(chosen_moves)
         print(turn, best_val, chosen_moves)
         if self.player_turn == PLAYER_ONE:
+            MOVES_ONE += len(chosen_moves)
             self.player_turn = PLAYER_TWO
         else:
+            MOVES_TWO += len(chosen_moves)
+            self.player_turn = PLAYER_ONE
+
+    def ai_heuristics_move(self, depth):
+        global MOVES_ONE, MOVES_TWO
+        maximizing = True
+        turn = 'Player\'s one choice: '
+        if self.player_turn == PLAYER_TWO:
+            maximizing = False
+            turn = 'Player\'s two choice: '
+        best_val, chosen_moves = Board.minimax_with_alpha_beta_heuristics(self, depth, -math.inf, math.inf, maximizing, self.player_turn, 0)
+        self.make_moves_from_list(chosen_moves)
+        print(turn, best_val, chosen_moves)
+        if self.player_turn == PLAYER_ONE:
+            MOVES_ONE += len(chosen_moves)
+            self.player_turn = PLAYER_TWO
+        else:
+            MOVES_TWO += len(chosen_moves)
             self.player_turn = PLAYER_ONE
 
     def make_moves_from_list(self, moves):
@@ -286,6 +326,56 @@ class Board:
 
             return best_val, chosen_moves
 
+    @staticmethod
+    def minimax_with_alpha_beta_heuristics(game, depth, alpha, beta, is_maximizing, starting_player, number_of_moves):
+        global COUNTER_FIRST, COUNTER_SECOND
+        chosen_moves = []
+        if depth == 0 or game.check_if_the_game_is_finished():
+            return Board.heuristic_evaluate(game, number_of_moves, starting_player), chosen_moves
+        if is_maximizing:
+            best_val = -math.inf
+            for available_moves in game.get_available_moves():
+                COUNTER_FIRST += 1
+                game_copy = copy.deepcopy(game)
+                game_copy.make_moves_from_list(available_moves)
+                starts = True
+                if starting_player == PLAYER_TWO:
+                    starts = False
+                if starts == is_maximizing:
+                    number_of_moves += len(available_moves)
+                result = Board.minimax_with_alpha_beta_heuristics(game_copy, depth - 1, alpha, beta, False, starting_player, number_of_moves)[0]
+                del game_copy
+                if result > best_val:
+                    best_val = result
+                    chosen_moves = available_moves
+
+                alpha = max(alpha, result)
+                if beta <= alpha:
+                    break
+            return best_val, chosen_moves
+        else:
+            best_val = math.inf
+            for available_moves in game.get_available_moves():
+                COUNTER_SECOND += 1
+                game_copy = copy.deepcopy(game)
+                game_copy.make_moves_from_list(available_moves)
+                starts = True
+                if starting_player == PLAYER_TWO:
+                    starts = False
+                if starts == is_maximizing:
+                    number_of_moves += len(available_moves)
+                result = Board.minimax_with_alpha_beta_heuristics(game_copy, depth - 1, alpha, beta, True, starting_player, number_of_moves)[0]
+                del game_copy
+                if result < best_val:
+                    best_val = result
+                    chosen_moves = available_moves
+
+                beta = min(beta, result)
+                if beta <= alpha:
+                    break
+
+            return best_val, chosen_moves
+
     def visualize_board(self):
         for hole in self.holes[1:7]:
             print(' ' + str(hole.stones) + ' ', end='')
@@ -333,9 +423,45 @@ class Board:
         print("AI 1 score: ", self.holes[7].stones)
         print("AI 2 score: ", self.holes[0].stones)
 
+    def ai_simulation_heuristics(self, depth):
+        last = self.ai_random_move()
+        self.check_if_stone_takes_opposite_hole(last)
+        self.decide_turn(last)
+        end = self.check_if_the_game_is_finished()
+        while not end:
+            Board.ai_heuristics_move(self, depth)
+            self.visualize_board()
+            end = self.check_if_the_game_is_finished()
+        print("The game has ended!")
+        print("AI 1 score: ", self.holes[7].stones)
+        print("AI 2 score: ", self.holes[0].stones)
+
 
 if __name__ == '__main__':
-    b = Board()
+    avg_time = 0
+    avg_moves = 0
+    iters = 5
+    for i in range(iters):
+        b = Board()
+        start = timer()
+        b.ai_simulation_minimax(3)
+        # b.ai_simulation_alpha_beta(3)
+        # b.ai_simulation_heuristics(4)
+        end = timer()
+        avg_time += (end - start)
+        print(end - start)
+        print(COUNTER_FIRST, COUNTER_SECOND)
+        if b.holes[0].stones > b.holes[7].stones:
+            avg_moves += COUNTER_SECOND
+        else:
+            avg_moves += COUNTER_FIRST
+        COUNTER_SECOND, COUNTER_FIRST = 0, 0
+        print('-----------------------------------')
+    print(avg_time / iters)
+    print(avg_moves / iters)
+    # b = Board()
+    # b1 = Board()
     # b.ai_simulation_minimax(3)
-    b.ai_simulation_alpha_beta(3)
-    print(COUNTER_FIRST, COUNTER_SECOND)
+    print('-------------------------')
+    print(MOVES_ONE / iters, MOVES_TWO / iters)
+    # b1.ai_simulation_heuristics(4)
